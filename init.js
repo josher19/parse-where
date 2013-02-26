19186c2e3b6cc0964da2e13344c86254f0ff6c8d
Josh W <josher19@users.sf.net>
2013-02-26 17:46:39 +0800

    Location API

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-function init(GLOBAL, app, rest) { 
+function init(GLOBAL, app, rest, $) { 
 	$.parse.init({
 		app_id : app, // <-- enter your Application Id here 
 		rest_key : rest // <--enter your REST API Key here	
@@ -7,4 +7,4 @@ function init(GLOBAL, app, rest) {
 	GLOBAL.rest_key = rest;
 }
 
-init(this, "PEv4zEZB1HGEpYuCEcugGkD8msNaounrwF0B0YkD", "50jH7UU4YO6p188ln20eZwEjr7rIveQd1huNh1Kc"); 
+init(this, "PEv4zEZB1HGEpYuCEcugGkD8msNaounrwF0B0YkD", "50jH7UU4YO6p188ln20eZwEjr7rIveQd1huNh1Kc", ("undefined" == typeof $) ? this : $); 
